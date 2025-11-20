7ddfb590ca095be8d240b92b3d851758b7994510
virajbhardwaj12345-max <viraj.bhardwaj12345@gmail.com>
2025-11-20 17:36:58 +0000

    datatypes

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log(typeof"vansh")
+console.log(typeof 20)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+
+//unefined-undefined
+//null-standable value
+//symbol-unique
+//boolean-true/false
+//string-""
+//undefined-
