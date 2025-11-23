ae3fb938e25989a7760cf06f300728c8a11c039e
virajbhardwaj12345-max <viraj.bhardwaj12345@gmail.com>
2025-11-23 20:25:04 +0000

    coversion of datatypes

---
+++ conve.js
@@ -0,0 +1,18 @@
+//let score ="hitesh"
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+ //let somenumber = 33
+ //let stringnumber = String(somenumber)
+ //console.log(stringnumber);
+ //console.log(typeof stringnumber);
+
+let Value = 3
+let negValue = -Value
+console.log(negValue);
+
+let str1="vansh"
+let str2="sharma"
+
+let str3=str1+str2
+console.log(str3);
