a43b2b1b7ed83f03e4ed9a52c59663a0c712bd9c
virajbhardwaj12345-max <viraj.bhardwaj12345@gmail.com>
2025-11-30 15:33:27 +0000

    comp

---
+++ comp.js
@@ -0,0 +1,15 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2!=1);
+//console.log(2==1);
+//console.log(2>=1);  
+
+//console.log("2"+2+2);
+//console.log(2+2+"2");
+
+console.log(undefined=0);
+console.log(undefined>0);
+console.log(undefined!=0);
+console.log(undefined<0);
+
+console.log("2"===2);
--- test.js
+++ test.js
@@ -10,3 +10,4 @@ console.log(typeof undefined)
 //boolean-true/false
 //string-""
 //undefined-
+
